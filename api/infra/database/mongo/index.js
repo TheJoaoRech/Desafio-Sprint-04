b4258c443f124541a3897363d557bd97f8e96e46
Graciela Parente <graciparentte@gmail.com>
2022-05-27 16:17:44 -0300

    feat: create connection with mongodb

---
+++ api/infra/database/mongo/index.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const config = require('../../../config/config');
+
+class Database {
+	constructor() {
+		this.connect();
+	}
+
+	connect(){
+		return mongoose.connect(
+			`mongodb//${config.database.host}/${config.database.collection}`,
+			{
+				user: config.database.username,
+				pass: config.database.password
+			}
+		);
+	}
+}
+
+module.exports = new Database().connect();
