ef0d2f3ad9b29cffaba0332a57ad24f254b9a34e
Alex Castelo <alexcastelo437@gmail.com>
2022-05-28 12:00:49 -0300

    feat: create GET BY ID product method

--- api/app/controller/ProductController.js
+++ api/app/controller/ProductController.js
@@ -16,8 +16,19 @@ class ProductController {
 		} catch (error) {
 			return res.status(400).json(error);
 		}
+	}
+
+	async getById(req, res) {
+		try {
+			const result = await ProductService.getById(req.params.id);
+			return res.status(200).json(result);
+		} catch (error) {
+			return res.status(400).json(error);
+		}
 
 	}
+
+
 
 }
 module.exports = new ProductController ();
--- api/routes/ProductRouter.js
+++ api/routes/ProductRouter.js
@@ -3,6 +3,7 @@ const ProductController = require('../app/controller/ProductController');
 module.exports = (server, routes, prefix = '/api/v1/product') => {
 	routes.post('/', ProductController.create);
 	routes.get('/', ProductController.listAll);
+	routes.get('/:id', ProductController.getById);
 
 	server.use(prefix, routes);
 };
