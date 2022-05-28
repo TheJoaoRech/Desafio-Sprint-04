1e01d252cc0b145dd6af468134c30887ad73608a
Graciela Parente <graciparentte@gmail.com>
2022-05-28 00:09:40 -0300

    feat: create product schema

---
+++ api/app/schema/ProductSchema.js
@@ -0,0 +1,22 @@
+const mongoose = require('mongoose'); 
+
+
+const ProductSchema = new mongoose.Schema({
+	name: {
+		type: String,
+		required: true
+	},
+	category: {
+		type: String,
+		required: true,
+	},
+	price: {
+		type: String,
+		required: true
+	},	
+},
+{versionKey: false}
+);
+
+const Product = mongoose.model('Product', ProductSchema);
+module.exports = Product;
