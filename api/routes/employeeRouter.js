5c177dcf18c9e8ff3306de536c9ab7c4da0da08a
Graciela Parente <graciparentte@gmail.com>
2022-05-27 23:25:40 -0300

    feat: create POST employee method

--- api/routes/employeeRouter.js
+++ api/routes/employeeRouter.js
@@ -1,10 +1,7 @@
 const EmployeeController = require('../app/controller/EmployeeController');
 
-module.exports = (server, routes, prefix = 'api/v1/people') => {
+module.exports = (server, routes, prefix = '/api/v1/employee') => {
 	routes.post('/', EmployeeController.create);
 
-	routes.get('/teste', (req,res) => {
-		res.json('teste');
-	});
 	server.use(prefix, routes);
 };
