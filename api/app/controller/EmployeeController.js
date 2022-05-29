38d711c606524b5eb6df2c9c4052a61a73627a35
João <joaooliveirarech@hotmail.com>
2022-05-29 00:04:46 -0400

    feat: Create getbyid, update and delete employee methods

--- api/app/controller/EmployeeController.js
+++ api/app/controller/EmployeeController.js
@@ -1,7 +1,6 @@
-//respostas dos metodos 
 const EmployeeService = require('../service/EmployeeService');
 class EmployeeController {
-	//create employe
+
 	async create(req, res) {
 		try {
 			const result = await EmployeeService.create(req.body);
@@ -20,5 +19,34 @@ class EmployeeController {
 		}
 	}
 
+	async getById(req, res) {
+		try {
+			const result = await EmployeeService.getById(req.params.id);
+			return res.status(200).json(result);
+		} catch (error) {
+			return res.status(400).json(error);
+		}
+
+	}
+
+	async updateEmployee(req, res) {
+		try {
+			const result = await EmployeeService.update(req.params.id, req.body);
+			return res.status(200).json(result);
+		} catch (error) {
+			return res.status(400).json(error);
+		}
+	
+	}
+
+	async deleteEmployee(req, res) {
+		try {
+			const result = await EmployeeService.delete(req.params.id);
+			return res.status(204).json(result);
+		} catch (error) {
+			return res.status(400).json(error);
+		}
+
+	}
 }
 module.exports = new EmployeeController ();
