d17a9a522c1b751eb3b634d888b3e41c28203c2c
João <joaooliveirarech@hotmail.com>
2022-05-29 00:06:28 -0400

    feat: create routes getbyid, update and delete on employee

--- api/routes/employeeRouter.js
+++ api/routes/employeeRouter.js
@@ -3,6 +3,9 @@ const EmployeeController = require('../app/controller/EmployeeController');
 module.exports = (server, routes, prefix = '/api/v1/employee') => {
 	routes.post('/', EmployeeController.create);
 	routes.get('/', EmployeeController.listAll);
+	routes.get('/:id', EmployeeController.getById);
+	routes.put('/:id', EmployeeController.updateEmployee);
+	routes.delete('/:id', EmployeeController.deleteEmployee);
 
 	server.use(prefix, routes);
 };
